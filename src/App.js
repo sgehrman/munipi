7b52172575cee1966f81948ee80cbda7c9319708
Steve Gehrman <sgehrman@gmail.com>
2019-05-22 02:00:08 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [contents, setContents] = useState(null);
+
+  async function refresh() {
+    const inbound =
+      "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&r=N_OWL&s=5206";
+
+    const result = await axios.get(inbound);
+
+    const direction = result.data.predictions.direction;
+
+    if (direction) {
+      const predictions = direction.prediction;
+
+      const times = [];
+
+      predictions.forEach(direction => {
+        times.push(parseInt(direction.minutes));
+      });
+
+      let time1 = "";
+      let time2 = "";
+
+      times.sort();
+
+      if (times.length > 0) {
+        time1 = times[0] + " mins";
+      }
+      if (times.length > 1) {
+        time2 = times[1] + " mins";
+      }
+
+      setContents(
+        <div>
+          <div>N Inbound</div>
+          <div className="time">{time1}</div>
+          <div className="time">{time2}</div>
+        </div>
+      );
+    }
+  }
+
+  useEffect(() => {
+    refresh();
+
+    const timer = setInterval(() => {
+      refresh();
+    }, 10000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return <div className="App">{contents}</div>;
 }
 
 export default App;
